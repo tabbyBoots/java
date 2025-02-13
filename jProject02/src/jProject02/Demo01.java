d86d50425d8362fefa6399441b4dfe4740682bd5
Boots <githubBoots@bloomski.com>
2025-02-14 00:47:36 +0800

    114-02-14 update from home

--- jProject02/src/jProject02/Demo01.java
+++ jProject02/src/jProject02/Demo01.java
@@ -4,7 +4,12 @@ public class Demo01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("NEW");
+		display();
+		System.out.println("END");
 
 	}
-
+	public static void display() {
+		System.out.println("display()");
+	}
 }
